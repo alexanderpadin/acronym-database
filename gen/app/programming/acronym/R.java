ffe59966c4b63045c7a7d844514e27696e2da3ba
Alexander Padin <alexander.padin91@gmail.com>
2014-10-13 21:25:13 -0400

    Release version

--- gen/app/programming/acronym/R.java
+++ gen/app/programming/acronym/R.java
@@ -1077,14 +1077,18 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int cancel=0x7f020057;
-        public static final int ic_launcher=0x7f020058;
-        public static final int line=0x7f020059;
-        public static final int search=0x7f02005a;
-        public static final int search_bar=0x7f02005b;
-        public static final int trasnparent=0x7f02005c;
+        public static final int actionbar_bg=0x7f020057;
+        public static final int add=0x7f020058;
+        public static final int cancel=0x7f020059;
+        public static final int ic_launcher=0x7f02005a;
+        public static final int line=0x7f02005b;
+        public static final int report=0x7f02005c;
+        public static final int search=0x7f02005d;
+        public static final int search_bar=0x7f02005e;
+        public static final int trasnparent=0x7f02005f;
     }
     public static final class id {
+        public static final int RelativeLayout01=0x7f050041;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1096,18 +1100,21 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
+        public static final int add=0x7f05004b;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int button1=0x7f050048;
         public static final int cancelButton=0x7f05003e;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
-        public static final int details_text=0x7f050042;
+        public static final int details_text=0x7f05004a;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
+        public static final int editText1=0x7f050045;
+        public static final int editText2=0x7f050047;
         public static final int edit_query=0x7f050030;
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
@@ -1118,17 +1125,22 @@ containing a value of this type.
         public static final int id_search_EditText=0x7f05003d;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView2=0x7f05003f;
+        public static final int imageView2=0x7f050040;
+        public static final int item=0x7f050049;
         public static final int listMode=0x7f050001;
+        public static final int listView_sugg=0x7f050042;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int not_sugg=0x7f050043;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
         public static final int relativeLayout1=0x7f05003c;
+        public static final int relativeLayout2=0x7f05003f;
+        public static final int report=0x7f05004c;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1146,8 +1158,8 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f050041;
-        public static final int textView2=0x7f050040;
+        public static final int textView1=0x7f050044;
+        public static final int textView2=0x7f050046;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
@@ -1205,8 +1217,12 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int notification=0x7f030019;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
+        public static final int add=0x7f030019;
+        public static final int item=0x7f03001a;
+        public static final int notification=0x7f03001b;
+        public static final int report=0x7f03001c;
+        public static final int suggestions=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
@@ -1285,6 +1301,12 @@ containing a value of this type.
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f0b008c;
+        /**  the theme applied to the application or activity 
+         */
+        public static final int CustomActionBarTheme=0x7f0b008d;
+        /**  ActionBar styles 
+         */
+        public static final int MyActionBar=0x7f0b008e;
         /**  Mimic text appearance in select_dialog_item.xml 
          */
         public static final int TextAppearance_AppCompat_Base_CompactMenu_Dialog=0x7f0b0063;
@@ -1487,6 +1509,7 @@ containing a value of this type.
         public static final int Widget_AppCompat_ProgressBar=0x7f0b000a;
         public static final int Widget_AppCompat_ProgressBar_Horizontal=0x7f0b0009;
         public static final int Widget_AppCompat_Spinner_DropDown_ActionBar=0x7f0b0022;
+        public static final int itemB=0x7f0b008f;
     }
     public static final class styleable {
         /**  ============================================ 
--- src/app/programming/acronym/MainActivity.java
+++ src/app/programming/acronym/MainActivity.java
@@ -1,5 +1,7 @@
 package app.programming.acronym;
 
+//Add functionality to report acrnym
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,9 +15,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -23,26 +30,44 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
+	Adapter adapter;
+	
 	EditText searchBar;
 	ImageButton cancelButton;
 	String DETAILS = "";
 	Context ctx = this;
 	Map<String, String> dataMap;
+	ListView list;
+	TextView no_sugg;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int actionBarTitleId = Resources.getSystem().getIdentifier("action_bar_title", "id", "android");
+        if (actionBarTitleId > 0) {
+            TextView title = (TextView) findViewById(actionBarTitleId);
+            if (title != null) {
+                Typeface typeface = Typeface.createFromAsset(this.getAssets(), "Roboto-Light.ttf");
+            	title.setTypeface(typeface);
+            }
+        }
         
         if(!haveData()) {
         	new generateData().execute();
@@ -50,22 +75,33 @@ public class MainActivity extends ActionBarActivity {
         	prepareData();
         	new checkUpdates().execute();
         }
-        
+        list = (ListView) findViewById(R.id.listView_sugg);
         searchBar = (EditText) findViewById(R.id.id_search_EditText);
         cancelButton = (ImageButton) findViewById(R.id.cancelButton);
+        no_sugg = (TextView) findViewById(R.id.not_sugg);
         
         searchBar.addTextChangedListener(new TextWatcher(){
             @SuppressLint({ "NewApi", "DefaultLocale" }) 
             public void afterTextChanged(Editable s) {
                 if(searchBar.getText().toString().length() > 0) {
                 	cancelButton.setImageResource(R.drawable.cancel);
-                	String acronymResult = dataMap.get((searchBar.getText().toString().toUpperCase()));
+                	final Map<String, String> temMap = new HashMap<String, String>();
+                	for (Map.Entry<String, String> entry : dataMap.entrySet()) {
+                		if(entry.getKey().toUpperCase().contains(searchBar.getText().toString().toUpperCase())) {
+                			temMap.put(entry.getKey(), entry.getValue());
+                		}
+                	}
                 	
-                	if (acronymResult != null && !acronymResult.isEmpty()){
-                		Toast.makeText(getApplicationContext(), ""+ acronymResult, Toast.LENGTH_LONG).show();
+                	if (!temMap.isEmpty()){
+                		list.setVisibility(View.VISIBLE);
+                		showSuggestions(temMap);
+                		no_sugg.setVisibility(View.GONE);
+                		
                 	}
                  } else {
-                	 cancelButton.setImageResource(R.drawable.trasnparent); 
+                	 cancelButton.setImageResource(R.drawable.trasnparent);
+                	 no_sugg.setVisibility(View.VISIBLE);
+                	 list.setVisibility(View.GONE);
                  }
             }
             public void beforeTextChanged(CharSequence s, int start, int count, int after){}
@@ -81,17 +117,25 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.add:
+            	openAddDlg();
+                return true;
+            case R.id.report:
+            	openReportDlg();
+            	return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
     
     private void prepareData() {
@@ -198,6 +242,81 @@ public class MainActivity extends ActionBarActivity {
         
      	
     }
+    
+    public void showSuggestions(Map<String, String> map) {
+        final Adapter adapter = new Adapter(map);
+        list.setAdapter(adapter);
+        
+        list.setOnItemClickListener(new OnItemClickListener() {
+	    	  @Override
+	    	  public void onItemClick(AdapterView<?> parent, View view,
+	    	    int position, long id) {	
+	    		  String query = adapter.getItem(position).getValue();
+	    		  query = query.replaceAll(" ", "%20");
+	    		  Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.comsearch?q=whats%20"
+	    				  + query));
+	    		  startActivity(browserIntent); 
+	    	  }
+	    	}); 
+    }
+    
+    private void openAddDlg() {
+		final Dialog dlg = new Dialog(ctx);
+    	dlg.setContentView(R.layout.add);
+    	dlg.setCanceledOnTouchOutside(true);
+    	dlg.setTitle("Add New Acronym");
+    	Button btn =  (Button) dlg.findViewById(R.id.button1);
+    	final EditText acrnym = (EditText) dlg.findViewById(R.id.editText1);
+    	final EditText def = (EditText) dlg.findViewById(R.id.editText2);
+    	
+    	
+    	btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(Intent.ACTION_SEND);
+				i.setType("message/rfc822");
+				i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"alexander.padin91@gmail.com"});
+				i.putExtra(Intent.EXTRA_SUBJECT, "Acronym++ ADD");
+				i.putExtra(Intent.EXTRA_TEXT   , "" + acrnym.getText() + "  -  " + def.getText());
+				try {
+				    startActivity(Intent.createChooser(i, "Send mail..."));
+				} catch (android.content.ActivityNotFoundException ex) {
+				    Toast.makeText(ctx, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+				}
+				dlg.dismiss();
+			}
+    	});
+    	dlg.show();
+	}
+    
+    private void openReportDlg() {
+		final Dialog dlg = new Dialog(ctx);
+    	dlg.setContentView(R.layout.report);
+    	dlg.setCanceledOnTouchOutside(true);
+    	dlg.setTitle("Report Error");
+    	Button btn =  (Button) dlg.findViewById(R.id.button1);
+    	final EditText acrnym = (EditText) dlg.findViewById(R.id.editText1);
+    	final EditText error = (EditText) dlg.findViewById(R.id.editText2);
+    	
+    	
+    	btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(Intent.ACTION_SEND);
+				i.setType("message/rfc822");
+				i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"alexander.padin91@gmail.com"});
+				i.putExtra(Intent.EXTRA_SUBJECT, "Acronym++ REPORT");
+				i.putExtra(Intent.EXTRA_TEXT   , "Error: " + acrnym.getText() + "  -  " + error.getText());
+				try {
+				    startActivity(Intent.createChooser(i, "Send mail..."));
+				} catch (android.content.ActivityNotFoundException ex) {
+				    Toast.makeText(ctx, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+				}
+				dlg.dismiss();
+			}
+    	});
+    	dlg.show();
+	}
     
     private class generateData extends AsyncTask<Void, Void, String>{
     	private ProgressDialog progressDialog;
